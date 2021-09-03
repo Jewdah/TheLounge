86f41d01063a171cab8e9f8fe97c0491f159b086
Edisson villabon <edissonvillabon@gmail.com>
2021-09-02 22:34:06 -0500

    Envio primer ejemplo de Post

--- index.js
+++ index.js
@@ -5,25 +5,9 @@ const app = express()
 var jsonParser = bodyParser.json()
 var urlencodedParser = bodyParser.urlencoded({ extended: false })
 
-
-app.get("/", function (req, res) {
- res.send("Hello World")
-})
-
-app.get("/Spanish", function (req, res) {
-    res.send("Hola mundo")
-   })
-
-app.get("/Italiano", function (req, res) {
-    res.send("Ciao mondo")
-   })
-
-
-
 app.post('/', urlencodedParser, function (req, res) {
     console.log(req.body.username)
     res.end()
   })
-
 
 app.listen(3000)
