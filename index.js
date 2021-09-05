27daba0f89e053a632f80d03edb9656031965882
Edisson villabon <edissonvillabon@gmail.com>
2021-09-04 23:37:21 -0500

    Envio 5 ejemplos de post

--- index.js
+++ index.js
@@ -6,8 +6,31 @@ var jsonParser = bodyParser.json()
 var urlencodedParser = bodyParser.urlencoded({ extended: false })
 
 app.post('/', urlencodedParser, function (req, res) {
-    console.log(req.body.Departamentos_de_Colombia)
-    res.end()
-  })
+  console.log(req.body.Lenguajes_de_programacion_1)
+  res.end()
+})
+
+app.post('/', urlencodedParser, function (req, res) {
+  console.log(req.body.Lenguajes_de_programacion_2)
+  res.end()
+})
+
+app.post('/', urlencodedParser, function (req, res) {
+  console.log(req.body.Lenguajes_de_programacion_3)
+  res.end()
+})
+
+app.post('/', urlencodedParser, function (req, res) {
+  console.log(req.body.Lenguajes_de_programacion_4)
+  res.end()
+})
+
+app.post('/', urlencodedParser, function (req, res) {
+  console.log(req.body.Lenguajes_de_programacion_5)
+  res.end()
+})
+  
+
+ 
 
 app.listen(3000)
